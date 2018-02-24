7082c4bed36c77be71f268cc6bf642f0f22bc2fd
nfoppiani <nicolo.foppiani@gmail.com>
2018-02-23 21:39:24 -0600

    Analyzer includes clusters now + new xml files

--- hitsanalysis/HitsAnalyzer_module.cc
+++ hitsanalysis/HitsAnalyzer_module.cc
@@ -7,10 +7,8 @@
 // from cetlib version v1_21_00.
 ////////////////////////////////////////////////////////////////////////
 
-
 #include <cstdlib>
 #include <iostream>
-
 
 #include "art/Framework/Core/EDAnalyzer.h"
 #include "art/Framework/Core/ModuleMacros.h"
@@ -39,63 +37,75 @@
 
 class HitsAnalyzer;
 
-
-class HitsAnalyzer : public art::EDAnalyzer {
+class HitsAnalyzer : public art::EDAnalyzer
+{
 public:
-  explicit HitsAnalyzer(fhicl::ParameterSet const & p);
+  explicit HitsAnalyzer(fhicl::ParameterSet const &p);
   // The compiler-generated destructor is fine for non-base
   // classes without bare pointers or other resource use.
 
   // Plugins should not be copied or assigned.
   HitsAnalyzer(HitsAnalyzer const &) = delete;
   HitsAnalyzer(HitsAnalyzer &&) = delete;
-  HitsAnalyzer & operator = (HitsAnalyzer const &) = delete;
-  HitsAnalyzer & operator = (HitsAnalyzer &&) = delete;
+  HitsAnalyzer &operator=(HitsAnalyzer const &) = delete;
+  HitsAnalyzer &operator=(HitsAnalyzer &&) = delete;
 
   // Required functions.
-  void analyze(art::Event const & e) override;
+  void analyze(art::Event const &e) override;
 
 private:
-
   std::string _pfp_producer = "pandoraNu";
 
-  TTree * fVerticesTree;
+  TTree *fPFParticlesTree;
   int fPdgCode;
-  double fvx, fvy, fvz, fstartx, fstarty, fstartz;
+  double fvx, fvy, fvz;
   int fNhits, fNclusters;
+  int fNhitsU, fNhitsV, fNhitsY;
   int fRun, fSubrun, fEvent;
 
-  TTree * fHitsTree;
+  TTree *fClustersTree;
+  double fClusterCharge, fClusterWidth, fClusterPosition;
+  int fClusterNhits, fClusterPlane;
+
+  TTree *fHitsTree;
   int fPlane;
   int fWire;
   double fCharge;
 
-  TTree * fSpacePointsTree;
+  TTree *fSpacePointsTree;
   double fx, fy, fz, fChargeU, fChargeV, fChargeY;
 };
 
-
-HitsAnalyzer::HitsAnalyzer(fhicl::ParameterSet const & p)
-  :
-  EDAnalyzer(p)  // ,
- // More initializers here.
+HitsAnalyzer::HitsAnalyzer(fhicl::ParameterSet const &p)
+    : EDAnalyzer(p) // ,
+                    // More initializers here.
 {
   art::ServiceHandle<art::TFileService> tfs;
-  
-  fVerticesTree = tfs->make<TTree>("Vertices", "Vertices Tree");
-  fVerticesTree->Branch("pdg_code", &fPdgCode, "pdg_code/i");
-  fVerticesTree->Branch("vx", &fvx, "vx/d");
-  fVerticesTree->Branch("vy", &fvy, "vy/d");
-  fVerticesTree->Branch("vz", &fvz, "vz/d");
-  fVerticesTree->Branch("start_x", &fstartx, "start_x/d");
-  fVerticesTree->Branch("start_y", &fstarty, "start_y/d");
-  fVerticesTree->Branch("start_z", &fstartz, "start_z/d");
-  fVerticesTree->Branch("n_hits", &fNhits, "n_hits/i");
-  fVerticesTree->Branch("n_clusters", &fNclusters, "n_clusters/i");
-  fVerticesTree->Branch("event", &fEvent, "event/i");
-  fVerticesTree->Branch("run", &fRun, "run/i");
-  fVerticesTree->Branch("subrun", &fSubrun, "subrun/i");
 
+  fPFParticlesTree = tfs->make<TTree>("PFParticles", "PFParticles Tree");
+  fPFParticlesTree->Branch("pdg_code", &fPdgCode, "pdg_code/i");
+  fPFParticlesTree->Branch("vx", &fvx, "vx/d");
+  fPFParticlesTree->Branch("vy", &fvy, "vy/d");
+  fPFParticlesTree->Branch("vz", &fvz, "vz/d");
+  fPFParticlesTree->Branch("n_hits", &fNhits, "n_hits/i");
+  fPFParticlesTree->Branch("n_hitsU", &fNhitsU, "n_hitsU/i");
+  fPFParticlesTree->Branch("n_hitsV", &fNhitsV, "n_hitsV/i");
+  fPFParticlesTree->Branch("n_hitsY", &fNhitsY, "n_hitsY/i");
+  fPFParticlesTree->Branch("n_clusters", &fNclusters, "n_clusters/i");
+  fPFParticlesTree->Branch("event", &fEvent, "event/i");
+  fPFParticlesTree->Branch("run", &fRun, "run/i");
+  fPFParticlesTree->Branch("subrun", &fSubrun, "subrun/i");
+
+  fClustersTree = tfs->make<TTree>("Clusters", "Clusters Tree");
+  fClustersTree->Branch("pdg_code", &fPdgCode, "pdg_code/i");
+  fClustersTree->Branch("charge", &fClusterCharge, "charge/d");
+  fClustersTree->Branch("width", &fClusterWidth, "width/d");
+  fClustersTree->Branch("n_hits", &fClusterNhits, "n_hits/i");
+  fClustersTree->Branch("plane", &fClusterPlane, "plane/i");
+  fClustersTree->Branch("position", &fClusterPosition, "position/d");
+  fClustersTree->Branch("event", &fEvent, "event/i");
+  fClustersTree->Branch("run", &fRun, "run/i");
+  fClustersTree->Branch("subrun", &fSubrun, "subrun/i");
 
   fHitsTree = tfs->make<TTree>("Hits", "Hits Tree");
   fHitsTree->Branch("pdg_code", &fPdgCode, "pdg_code/i");
@@ -119,18 +129,18 @@ HitsAnalyzer::HitsAnalyzer(fhicl::ParameterSet const & p)
   fSpacePointsTree->Branch("subrun", &fSubrun, "subrun/i");
 }
 
-void HitsAnalyzer::analyze(art::Event const & evt)
+void HitsAnalyzer::analyze(art::Event const &evt)
 {
   fRun = evt.run();
   fSubrun = evt.subRun();
   fEvent = evt.id().event();
 
-  auto const &pfparticle_handle = 
-    evt.getValidHandle<std::vector<recob::PFParticle>>(_pfp_producer);
+  auto const &pfparticle_handle =
+      evt.getValidHandle<std::vector<recob::PFParticle>>(_pfp_producer);
   auto const &cluster_handle =
       evt.getValidHandle<std::vector<recob::Cluster>>(_pfp_producer);
-  auto const &spacepoint_handle = 
-    evt.getValidHandle<std::vector<recob::SpacePoint>>(_pfp_producer);
+  auto const &spacepoint_handle =
+      evt.getValidHandle<std::vector<recob::SpacePoint>>(_pfp_producer);
 
   art::FindOneP<recob::Vertex> vertex_per_pfpart(pfparticle_handle, evt, _pfp_producer);
   art::FindOneP<recob::Shower> shower_per_pfpart(pfparticle_handle, evt, _pfp_producer);
@@ -145,54 +155,26 @@ void HitsAnalyzer::analyze(art::Event const & evt)
     recob::PFParticle const &pfparticle = pfparticle_handle->at(i_pfp);
     fPdgCode = pfparticle.PdgCode();
 
-    if (fPdgCode == 11)
-    {
-      try
-      {
-        art::Ptr<recob::Shower> pf_obj = shower_per_pfpart.at(i_pfp);
-        fstartx = pf_obj->ShowerStart().X();
-        fstarty = pf_obj->ShowerStart().Y();
-        fstartz = pf_obj->ShowerStart().Z();
-      }
-      catch (...)
-      {
-        fstartx = 1000000.;
-        fstarty = 1000000.;
-        fstartz = 1000000.;
-        std::cout << "No start point found for shower " << fPdgCode << std::endl;
-      }
-    }
-    else if (fPdgCode == 13)
-    {
-      try
-      {
-        art::Ptr<recob::Track> pf_obj = track_per_pfpart.at(i_pfp);
-        fstartx = pf_obj->Start().X();
-        fstarty = pf_obj->Start().Y();
-        fstartz = pf_obj->Start().Z();
-      }
-      catch (...)
-      {
-        fstartx = 1000000.;
-        fstarty = 1000000.;
-        fstartz = 1000000.;
-        std::cout << "No start point found for track " << fPdgCode << std::endl;
-      }
-    }
-    else
-    {
-      fstartx = 1000000.;
-      fstarty = 1000000.;
-      fstartz = 1000000.;
-    }
-      
-    fNhits = 0;    
-    fNclusters = 0;   
-    // Hits
+    fNhits = 0;
+    fNhitsU = 0;
+    fNhitsV = 0;
+    fNhitsY = 0;
+    fNclusters = 0;
+
+    // Clusters and Hits
     std::vector<art::Ptr<recob::Cluster>> clusters = clusters_per_pfpart.at(i_pfp);
+    fNclusters = clusters.size();
     for (art::Ptr<recob::Cluster> &cluster : clusters)
     {
-      fNclusters += 1;
+      fClusterCharge = cluster->Integral();
+      fClusterWidth = cluster->Width();
+      fClusterNhits = cluster->NHits();
+      fClusterPlane = cluster->View();
+      fClusterPosition = (cluster->EndWire() + cluster->StartWire()) / 2.;
+      fClustersTree->Fill();
+
+      std::cout << "cluster for " << fPdgCode << " with " << fClusterCharge << "   " << cluster->Integral() << fClusterNhits << "   " << cluster->NHits() << std::endl;
+
       std::vector<art::Ptr<recob::Hit>> hits = hits_per_cluster.at(cluster.key());
       fNhits += hits.size();
       for (art::Ptr<recob::Hit> &hit : hits)
@@ -201,17 +183,30 @@ void HitsAnalyzer::analyze(art::Event const & evt)
         fWire = hit->WireID().Wire;
         fCharge = hit->Integral();
         fHitsTree->Fill();
+
+        if (fPlane == 0)
+        {
+          fNhitsU += 1;
+        }
+        else if (fPlane == 1)
+        {
+          fNhitsV += 1;
+        }
+        else if (fPlane == 2)
+        {
+          fNhitsY += 1;
+        }
       }
     }
 
     try
     {
       art::Ptr<recob::Vertex> vertex_obj = vertex_per_pfpart.at(i_pfp);
-      double reco_neutrino_vertex[3];
-      vertex_obj->XYZ(reco_neutrino_vertex);
-      fvx = reco_neutrino_vertex[0];
-      fvy = reco_neutrino_vertex[1];
-      fvz = reco_neutrino_vertex[2];
+      double neutrino_vertex[3];
+      vertex_obj->XYZ(neutrino_vertex);
+      fvx = neutrino_vertex[0];
+      fvy = neutrino_vertex[1];
+      fvz = neutrino_vertex[2];
     }
     catch (...)
     {
@@ -221,17 +216,17 @@ void HitsAnalyzer::analyze(art::Event const & evt)
       std::cout << "No vertex found for " << fPdgCode << " with " << fNhits << std::endl;
     }
 
-    fVerticesTree->Fill();
+    fPFParticlesTree->Fill();
 
     // Space points
     std::vector<art::Ptr<recob::SpacePoint>> spcpnts = spcpnts_per_pfpart.at(i_pfp);
     for (art::Ptr<recob::SpacePoint> &sps : spcpnts)
     {
       auto xyz = sps->XYZ();
-      fx = xyz[0]; 
+      fx = xyz[0];
       fy = xyz[1];
       fz = xyz[2];
-      
+
       std::vector<art::Ptr<recob::Hit>> hits = hits_per_spcpnts.at(sps.key());
       fChargeU = 0;
       fChargeV = 0;
@@ -240,16 +235,18 @@ void HitsAnalyzer::analyze(art::Event const & evt)
       {
         double hit_plane = hit->WireID().Plane;
         double hit_integral = hit->Integral();
-        if (hit_plane == 0) fChargeU += hit_integral;
-        else if (hit_plane == 1) fChargeV += hit_integral;
-        else if (hit_plane == 2) fChargeY += hit_integral;
-        else std::cout << "hit plane != 0, 1, 2, but " << hit_plane << std::endl; 
+        if (hit_plane == 0)
+          fChargeU += hit_integral;
+        else if (hit_plane == 1)
+          fChargeV += hit_integral;
+        else if (hit_plane == 2)
+          fChargeY += hit_integral;
+        else
+          std::cout << "hit plane != 0, 1, 2, but " << hit_plane << std::endl;
       }
       fSpacePointsTree->Fill();
     }
   }
-
-
 }
 
 DEFINE_ART_MODULE(HitsAnalyzer)
